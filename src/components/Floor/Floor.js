7872ba6623fb178a555ad0f75754044b35b20032
olecrity <oleh.kazmirchuk.pz.2022@lpnu.ua>
2024-11-05 00:35:15 +0200

    Added componets for close view on Floor

---
+++ src/components/Floor/Floor.js
@@ -0,0 +1,11 @@
+import FloorSchema from "./FloorSchema/FloorSchema";
+
+function Floor() {
+  return (
+    <div>
+      <FloorSchema />
+    </div>
+  );
+}
+
+export default Floor;
---
+++ src/components/Floor/FloorSchema/FloorSchema.js
@@ -0,0 +1,6 @@
+import styles from "./FloorSchema.module.scss";
+function FloorSchema() {
+  return <div className={styles.FloorSchema}></div>;
+}
+
+export default FloorSchema;
