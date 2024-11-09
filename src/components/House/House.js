ca6194e69b25ff8b3ca5518b15487f1f873169ae
kol1a_ <koljanmuzyka2108@gmail.com>
2024-11-10 01:06:21 +0200

    added structure and style to Building page

--- src/components/House/House.js
+++ src/components/House/House.js
@@ -3,13 +3,81 @@ import styles from "./House.module.scss";
 
 function House({ floors }) {
   return (
-    <>
-      <div className={styles.house}>
-        {floors.map((floor, i) => (
-          <HouseFloor key={i} />
-        ))}
-      </div>
-    </>
+      <div className={styles.main_house}>
+        <div className={styles.simulation_section}>
+          <h3 className={styles.main_caption}>Simulation building</h3>
+          <div className={styles.house}>
+            {floors.map((floor, i) => (
+              <HouseFloor key={i} />
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.building_structure}>
+          <h3 className={styles.main_caption}>Create building structure</h3>
+          <div className={styles.create_table}>
+            <div className={styles.table_section}>
+              <table id={styles.floors_table}>
+                <tr>
+                <th scope="col" className={styles.caption}>Floor type</th>
+                <th scope="col" className={styles.floor_cancel}>Remove</th>
+                </tr>
+                <tbody id={styles.table_body}>
+
+                </tbody>
+              </table>
+            </div>
+            <div className={styles.buttons_section}>
+              <button className={styles.main_title_btn}>
+                  <a className={styles.navigation}>
+                      <p>Add T1 foor</p>
+                  </a>
+              </button>
+              <button className={styles.main_title_btn}>
+                  <a className={styles.navigation}>
+                      <p>Add T2 foor</p>
+                  </a>
+              </button>
+              <button id={styles.submit_struct_btn} className={styles.main_title_btn}>
+                  <a className={styles.navigation}>
+                      <p>Submit structure</p>
+                  </a>
+              </button>
+            </div>
+          </div>
+
+          <div className={styles.lower_section}>
+            <div className={styles.button_section2}>
+              <button className={styles.main_title_btn2}>
+                <a className={styles.navigation}>
+                    <p>Start simulation</p>
+                </a>
+              </button>
+              <button className={styles.main_title_btn2}>
+                  <a className={styles.navigation}>
+                      <p>Stop simulation</p>
+                  </a>
+              </button>
+              <button className={styles.main_title_btn2}>
+                <a className={styles.navigation}>
+                    <p>Resume simulation</p>
+                </a>
+              </button>
+              <button className={styles.main_title_btn2}>
+                  <a className={styles.navigation}>
+                      <p>Save information</p>
+                  </a>
+              </button>
+            </div>
+            <div className={styles.ststus_show}>
+              <h3 className={styles.main_caption}>Simulation status:</h3>
+              <p>In Process</p>
+            </div>
+          </div>
+          
+
+        </div>
+    </div>
   );
 }
 
--- src/components/House/HouseFloor/HouseFloor.js
+++ src/components/House/HouseFloor/HouseFloor.js
@@ -1,20 +1,31 @@
+import React from "react";
 import styles from "./HouseFloor.module.scss";
+
+
 function HouseFloor() {
   function handleMouseEnter(e) {
-    console.log(e);
     e.target.style.border = "5px solid black";
   }
 
   function handleMouseLeave(e) {
-    console.log(e);
-    e.target.style.border = "2px solid black";
+    e.target.style.border = "1px solid #35353500";
   }
   return (
     <div
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
+      // onMouseEnter={handleMouseEnter}
+      // onMouseLeave={handleMouseLeave}
       className={styles["house-floor"]}
-    ></div>
+    >
+     {[1, 2, 3, 4].map((_, index) => (
+        <div key={index} className={styles.sensor}>
+          <div className={`${styles.decoration} ${styles[`color-${index + 1}`]}`}></div>
+          <p className={`${styles.sensors_amount} ${styles[`type-${index + 1}`]}`}>
+            {index + 1}
+          </p>
+        </div>
+      ))}
+      
+    </div>
   );
 }
 
